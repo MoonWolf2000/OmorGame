064c33e76adb08ae69ebd598af47327be6e9a768
Lina <lv.zuhause@gmail.com>
2020-10-27 00:48:07 +0100

    Life can be reduced have problem with double acttivation

--- OmorGame_V0.0.2/Assets/Scripts/PlayerScripts/LifeController.cs
+++ OmorGame_V0.0.2/Assets/Scripts/PlayerScripts/LifeController.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class LifeController : MonoBehaviour
 {
     public int lifepoints = 15;
+    public List<int> lifechangers = new List<int>();
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+       
+
+        if (lifechangers.Count == 0) return;
+        foreach(int i in lifechangers)
+        {
+            lifepoints = lifepoints + i;
+
+        }
+
+        lifechangers.Clear();
     }
 }

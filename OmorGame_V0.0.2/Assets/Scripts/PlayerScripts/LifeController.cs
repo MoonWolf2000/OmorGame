c9e6521d3103b052f715c8d22f75c1a9f6773d68
Lina <lv.zuhause@gmail.com>
2020-10-26 23:35:04 +0100

    Add LifeController script

---
+++ OmorGame_V0.0.2/Assets/Scripts/PlayerScripts/LifeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeController : MonoBehaviour
+{
+    public int lifepoints = 15;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

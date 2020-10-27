afe4f8d546aaa4e81cd731b8f4150f337f1e7cb5
Lina <lv.zuhause@gmail.com>
2020-10-27 05:35:41 +0100

    Charactr rotation works now

--- OmorGame_V0.0.2/Assets/Scripts/PlayerScripts/MoveController.cs
+++ OmorGame_V0.0.2/Assets/Scripts/PlayerScripts/MoveController.cs
@@ -8,6 +8,10 @@ public class MoveController : MonoBehaviour
    public Vector2 movement;
     public float speed = 5f ;
     public Rigidbody2D rb;
+    public Transform t;
+  public  int futureDirection = 0;
+
+    public  Vector2[] directions = new Vector2[4];
 
     private void FixedUpdate()
     {
@@ -18,7 +22,53 @@ public class MoveController : MonoBehaviour
     public void Moving(InputAction.CallbackContext context)
     {
         Debug.Log("I wanna move ");
-        movement = context.action.ReadValue<Vector2>();   
+        movement = context.action.ReadValue<Vector2>();
+
+        OrientatioCheck(context.action.ReadValue<Vector2>());
+    }
+
+
+   private void OrientatioCheck(Vector2 input)
+    {
+        if (input == new Vector2(0, 0)) return;
+        int i = 0;
+
+        float min= 2;
+        foreach(Vector2 v in directions)
+        {
+            Debug.Log((input - v).magnitude);
+            if((input-v).magnitude <= min)
+            {
+                min = (input - v).magnitude;
+                futureDirection = i;
+            }
+            i++;
+        }
+        Debug.Log(futureDirection);
+        //Debug.LogError("StOOOP");
+        switch(futureDirection)
+        {
+            case 0:
+          
+                t.rotation = Quaternion.Euler(0f, 0f, 0f);
+                break;
+            case 1:
+                t.rotation = Quaternion.Euler(0f, 0f, 180f);
+                break;
+            case 2:
+                t.rotation = Quaternion.Euler(0f, 0f, 270f);
+                break;
+            case 3:
+                t.rotation = Quaternion.Euler(0f, 0f, 90f);
+                break;
+
+
+                //default:
+                //    t.rotation = new Quaternion(0f, 0f, -270f, 0f);
+                //    break;
+
+        }
+
 
     }
 

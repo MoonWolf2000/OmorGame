bb4f2beb47f0d5aaa103aac7a73bc7a5bf8cd885
Lina <lv.zuhause@gmail.com>
2020-11-03 14:49:27 +0100

    Added Potion Class

---
+++ OmorGame_V0.0.2/Assets/Scripts/Potions/Potion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Potion : MonoBehaviour
+{
+    public Element element;
+    public virtual void Drink()
+    {
+
+    }
+
+    public enum Element
+    {
+       elementA = 0,
+       elementB = 1,
+       elementC = 2,
+       elementD = 3
+    }
+}

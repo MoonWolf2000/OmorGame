43dca450cd2e53e8aced2b492de757750273922c
Chiara <59558162+MoonWolf2000@users.noreply.github.com>
2020-11-11 20:29:33 +0100

    kleine changes

--- OmorGame_V0.0.2/Assets/Scripts/PlayerScripts/Player.cs
+++ OmorGame_V0.0.2/Assets/Scripts/PlayerScripts/Player.cs
@@ -6,14 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-<<<<<<< HEAD
+
     public SceneAsset test;
-=======
+
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
     }
->>>>>>> 9dc4d6c72fc1745cab8f3f081979baffbfa71a6a
+
     // Start is called before the first frame update
     void Start()
     {
